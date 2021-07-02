cfb2f88dc86944a22fc2bccc067290bd1841ac39
Sebastian Simarro <ssebastian210@gmail.com>
2021-07-02 19:05:30 -0300

    prueba armado player

--- js/player.js
+++ js/player.js
@@ -1,7 +1,20 @@
 "use strict";
 
-let myAudio = document.getElementById("myAudio");
+/* 
+const radios = {
+  nombre: "Aspen",
+  dial: "102.3",
+  streamId: "https://mdstrm.com/audio/60a2745ff943100826374a70/icecast.audio",
+};
+
+let radioPlay = document.getElementById("play-button");
+let nombreRadio = document.querySelector("nombre--player");
+nombreRadio = radios.nombre + " " + radios.dial;
 
 function togglePlay() {
-  return myAudio.paused ? myAudio.play() : myAudio.pause();
+  return radioPlay.paused ? radioPlay.play() : radioPlay.pause();
+} */
+
+function playAudio(url) {
+  new Audio(url).play();
 }
